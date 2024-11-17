90803463c9fb42daa8261bff141bc2a464fe6757
unix-follower <61003331+unix-follower@users.noreply.github.com>
2024-11-17 17:01:05 +0300

    refactoring: rename files/variable/functions, move directories and etc (#11)
    Co-authored-by: Artsem_Nikitsenka <artsem_nikitsenka@epam.com>

--- scala/linalg/linear_systems/src/main/scala/org/example/math/linearsystem/package.scala
+++
@@ -1,9 +0,0 @@
-package org.example.math
-
-package object linearsystem {
-    object ArrayConstants {
-        val FIRST_COLUMN: Byte = 0
-        val SECOND_COLUMN: Byte = 1
-        val THIRD_COLUMN: Byte = 2
-    }
-}
